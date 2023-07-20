9853cadf5ff9c6a03f218f11c9e9c99f89886181
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-07-20 15:58:24 -0700

    completes lab-09

--- app.js
+++ app.js
@@ -5,19 +5,43 @@ const storeValues = [];
 const SALES_TABLE = document.getElementById('table-body');
 const HEAD_TABLE = document.getElementById('table-head');
 const FOOTER_TABLE = document.getElementById('table-footer');
+let formElement = document.getElementById('stand-form');
 
-// function randomCust() {
-//     return (Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
-// }
-
-function CookieStand(name, minCust, maxCust, avgCookie, sales) {
-    this.name = name;
+function CookieStand(location, minCust, maxCust, avgCookie, sales) {
+    this.location = location;
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgCookie = avgCookie;
     this.sales = sales;
-    // this.totals = totals;
 }
+
+CookieStand.prototype.sales = function () {
+        let hours = 14;
+        for (let i = 0; i < storeValues.length; i++) {
+            let currentLocation = storeValues[i];
+            let dailyTotal = 0;
+            for (let j = 0; j < hours; j++) {
+                let cookies = Math.floor(currentLocation.random() * currentLocation.avgCookie);
+                currentLocation.sales.push(cookies);
+                dailyTotal += cookies;
+            }
+            currentLocation.sales.push(dailyTotal);
+        }
+    };
+
+    CookieStand.prototype.calculateSales = function () {
+        let hours = 14;
+        for (let i = 0; i < storeValues.length; i++) {
+            let currentLocation = storeValues[i];
+            let dailyTotal = 0;
+            for (let j = 0; j < hours; j++) {
+                let cookies = Math.floor(currentLocation.random() * currentLocation.avgCookie);
+                currentLocation.sales.push(cookies);
+                dailyTotal += cookies;
+            }
+            currentLocation.sales.push(dailyTotal);
+        }
+    };
 
 CookieStand.prototype.random = function () {
     return (Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
@@ -28,14 +52,8 @@ CookieStand.prototype.hours = [' ', '6:00am', '7:00am', '8:00am', '9:00am', '10:
 
 CookieStand.prototype.render = function () {
     let rowElement = document.createElement('tr');
-    // let nameElement = document.createElement('th');
-    // nameElement.textContent = this.name;
-    // rowElement.appendChild(nameElement);
-    createHTML('th', this.name, rowElement);
+    createHTML('th', this.location, rowElement);
     for (let i = 0; i < this.sales.length; i++) {
-        // let salesElement = document.createElement('td');
-        // salesElement.textContent = this.sales[i];
-        // rowElement.appendChild(salesElement);
         createHTML('td', this.sales[i], rowElement);
     }
     SALES_TABLE.appendChild(rowElement);
@@ -59,118 +77,27 @@ let Dubai = new CookieStand('Dubai', 11, 38, 3.7, []);
 let Paris = new CookieStand('Paris', 20, 38, 2.3, []);
 let Lima = new CookieStand('Lima', 2, 16, 4.6, []);
 
-
-
-// const Seattle = {
-//     name: 'Seattle',
-//     minCust: 23,
-//     maxCust: 65,
-//     avgCookie: 6.3,
-//     random: randomCust,
-//     sales: []
-// };
-
-// const Tokyo = {
-//     name: 'Tokyo',
-//     minCust: 3,
-//     maxCust: 24,
-//     avgCookie: 1.2,
-//     random: randomCust,
-//     sales: []
-// };
-
-// const Dubai = {
-//     name: 'Dubai',
-//     minCust: 11,
-//     maxCust: 38,
-//     avgCookie: 3.7,
-//     random: randomCust,
-//     sales: []
-// };
-
-// const Paris = {
-//     name: 'Paris',
-//     minCust: 20,
-//     maxCust: 38,
-//     avgCookie: 2.3,
-//     random: randomCust,
-//     sales: []
-// };
-
-// const Lima = {
-//     name: 'Lima',
-//     minCust: 2,
-//     maxCust: 16,
-//     avgCookie: 4.6,
-//     random: randomCust,
-//     sales: []
-// };
-
 storeValues.push(Seattle, Tokyo, Dubai, Paris, Lima);
 
+CookieStand.prototype.calculateSales();
 
-// for (let i = 0; i < storeValues.length; i++) {
-//     let currentLocation = storeValues[i];
-//     let amHours = 11;
-//     let pmHours = 7;
-//     let sum = 0;
-//     for (let j = 6; j <= amHours; j++) {
-//         let cookies = Math.round(currentLocation.random() * currentLocation.avgCookie);
-//         currentLocation.sales.push( j + 'am: ' + cookies + ' cookies');
-//         sum += cookies;
-//     }
-//     for (let j = 12; j === 12; j++) {
-//         let cookies = Math.round(currentLocation.random() * currentLocation.avgCookie);
-//         currentLocation.sales.push( j + 'pm: ' + cookies + ' cookies');
-//         sum += cookies;
-//     }
-//     for (let j = 1; j <= pmHours; j++) {
-//         let cookies = Math.round(currentLocation.random() * currentLocation.avgCookie);
-//         currentLocation.sales.push( j + 'pm: ' + cookies + ' cookies');
-//         sum += cookies;
-//     }
-//     currentLocation.sales.push('Total: ' + sum + ' cookies');
-// }
-let hours = 14;
 for (let i = 0; i < storeValues.length; i++) {
-    let currentLocation = storeValues[i];
-    let dailyTotal = 0;
-    for (let j = 0; j < hours; j++) {
-        let cookies = Math.floor(currentLocation.random() * currentLocation.avgCookie);
-        currentLocation.sales.push(cookies);
-        dailyTotal += cookies;
-    }
-    currentLocation.sales.push(dailyTotal);
     storeValues[i].render();
 }
 
 let headerRow = document.createElement('tr');
 for (let i = 0; i < CookieStand.prototype.hours.length; i++) {
-    // let headerCell = document.createElement('th');
-    // headerCell.textContent = CookieStand.prototype.hours[i];
-    // headerRow.appendChild(headerCell);
     createHTML('th', CookieStand.prototype.hours[i], headerRow);
 }
 HEAD_TABLE.appendChild(headerRow);
 
-// for (let i = 0; i < storeValues.length; i++) {
-//     storeValues[i].render();
-//   }
-
 const hourlyTotals = calculateHourlyTotals();
-console.log(hourlyTotals);
 
 let footerRow = document.createElement('tr');
 for (let i = 0; i <= hourlyTotals.length - 1; i++) {
     if (i === 0) {
-    // let nameElement = document.createElement('th');
-    // nameElement.textContent = 'Totals';
-    // footerRow.appendChild(nameElement);
     createHTML('th', 'Totals', footerRow);
     }
-    // let footerCell = document.createElement('td');
-    // footerCell.textContent = hourlyTotals[i];
-    // footerRow.appendChild(footerCell);
     createHTML('td', hourlyTotals[i], footerRow);
 }
 FOOTER_TABLE.appendChild(footerRow);
@@ -179,18 +106,42 @@ function createHTML(elementToCreate, contentToAdd, elementToAddTo) {
     let element = document.createElement(elementToCreate);
     element.textContent += contentToAdd;
     elementToAddTo.appendChild(element);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(event.target.location.value);
+    console.log(event.target.minCust.value);
+    console.log(event.target.maxCust.value);
+    console.log(event.target.avgCookie.value);
+
+    let location = event.target.location.value;
+    let minCust = parseInt(event.target.minCust.value);
+    let maxCust = parseInt(event.target.maxCust.value);
+    let avgCookie = parseInt(event.target.avgCookie.value);
+    let sales = [];
+    let stand = new CookieStand(location, minCust, maxCust, avgCookie, sales);
+    storeValues.push(stand);
+    stand.calculateSales();
+
+    const hourlyTotals = calculateHourlyTotals();
+    FOOTER_TABLE.innerHTML = '';
+    let footerRow = document.createElement('tr');
+    for (let i = 0; i <= hourlyTotals.length - 1; i++) {
+        if (i === 0) {
+            createHTML('th', 'Totals', footerRow);
+        }
+        createHTML('td', hourlyTotals[i], footerRow);
+    }
+    FOOTER_TABLE.appendChild(footerRow);
+
+    // Clear the form inputs after submission
+    event.target.reset();
+
+    stand.render();
+    console.log(storeValues);
+
   }
-
-// const sectionElement = document.createElement('section');
-// document.body.appendChild(sectionElement);
-
-// for (let i = 0; i < storeValues.length; i++) {
-//     let currentLocation = storeValues[i];
-//     createHTML('h3', currentLocation.name, sectionElement);
-//     const unListElement = document.createElement('ul');
-//     sectionElement.appendChild(unListElement);
-
-//     for (let j = 0; j < currentLocation.sales.length; j++) {
-//         createHTML('li', currentLocation.sales[j], unListElement);
-//     }
-// }
+formElement.addEventListener('submit', handleSubmit);
+console.log(storeValues);
